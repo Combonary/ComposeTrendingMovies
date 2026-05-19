96f79545a01efc44efc8152f2d7e28ff918d2dc5
DESKTOP2250\pasca <pascaladjaero@gmail.com>
2026-05-19 18:43:08 -0500

    dependency updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.composetrendingmovies"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.composetrendingmovies"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
